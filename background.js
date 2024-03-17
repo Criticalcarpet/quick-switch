3848e763625ae8d3b764b9ca0d06a415d69908af
criticalcarpet <criticalcarpet.dev@gmail.com>
2024-03-17 09:35:18 +0530

    docs: document background.js

--- background.js
+++ background.js
@@ -1,11 +1,22 @@
 let toggle = false;
+
+/**
+ * @description To create tabs from listening to the injected script and message the script to remove itself
+ */
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-    if (message.action === 'performAPIOperation') {
-        chrome.tabs.create({ url: message.url });
-        chrome.action.setIcon({ path: "icons/icon-16.png" });
-        chrome.tabs.sendMessage(sender.tab.id, { message: "remove" });
-    }
+  if (message.action === "performAPIOperation") {
+    chrome.tabs.create({ url: message.url });
+    chrome.action.setIcon({ path: "icons/icon-16.png" });
+    chrome.tabs.sendMessage(sender.tab.id, { message: "remove" });
+  }
 });
+
+/**
+ * @description To toggle the quick switch feature
+ * @var toggle @type {Boolean} - true if the quick switch feature is on, false otherwise.
+ * @var tabResult @type {Array} - data of the current focused tab
+ * @var tabId @type {Number} - id of the current focused tab
+ */
 chrome.commands.onCommand.addListener(async (command) => {
   if (command === "quick-switch") {
     toggle = !toggle;
@@ -17,9 +28,9 @@ chrome.commands.onCommand.addListener(async (command) => {
     if (toggle) {
       chrome.action.setIcon({ path: "icons/active-16.png" });
       await chrome.scripting.executeScript({
-        target: {tabId : tabId, allFrames: true},
-        files: ["scripts/inject.js"]
-      })
+        target: { tabId: tabId, allFrames: true },
+        files: ["scripts/inject.js"],
+      });
       chrome.tabs.sendMessage(tabId, { message: toggle });
       toggle = false;
     } else {
